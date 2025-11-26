8773906189b1d9d4ce2fe83d59f1e88ad83a47ef
DiegoEscalanteITSON <diego.escalante272259@potros.itson.edu.mx>
2025-11-25 21:49:35 -0700

    Funciones

--- Proyecto Final/archivo_ventas.py
+++ Proyecto Final/archivo_ventas.py
@@ -1,1 +1,8 @@
 # Archivo que guarda la ventas en txt
+
+def guardar_venta_en_txt(ventas):
+    with open("ventas.txt", "w", encoding="utf-8") as archivo:
+        for venta in ventas:
+            archivo.write(f"{venta['nombre']} - ${venta['precio']}\n")
+            print(f"Venta guardada: {venta['nombre']} - ${venta['precio']}")
+            print("✔ Archivo ventas de registradas actualizado correctamente.")
