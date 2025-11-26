a87cb9e2327abd8b3dddb208cd4b4809a4c5a693
DiegoEscalanteITSON <diego.escalante272259@potros.itson.edu.mx>
2025-11-25 21:58:33 -0700

    Catalogo de juegos

--- Proyecto Final/catalogo.py
+++ Proyecto Final/catalogo.py
@@ -1,1 +1,9 @@
 # Archivo del catalogo que ser leido
+
+Catalogo = (
+    {1, "nombre": "DOOM Eternal", "precio": 200},
+    {2, "nombre": "Hollow Knight", "precio": 150},
+    {3, "nombre": "Fallout New Vegas", "precio": 300},
+    {4, "nombre": "Civilization VI", "precio": 250},
+    {5, "nombre": "Super Smash Bros Ultimate", "precio": 400},
+)
