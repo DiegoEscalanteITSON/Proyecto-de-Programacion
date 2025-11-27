e13403af1a4718551f7f296c8ad8e67c2c814a96
DiegoEscalanteITSON <diego.escalante272259@potros.itson.edu.mx>
2025-11-27 10:51:33 -0700

    perdon

--- Proyecto Final/gui_ventana.py
+++ Proyecto Final/gui_ventana.py
@@ -4,9 +4,30 @@ from tkinter import messagebox, ttk
 from archivo_ventas import guardar_venta_en_txt
 from def_ventas import agregar_venta
 
-def ventana_registro_ventas(ventana_principal):
-    ventana_registro_ventas = tk.Toplevel(ventana_principal)
-    ventana_registro_ventas.title("Registro de Ventas")
-    ventana_registro_ventas.geometry("400x300")
+def adrir_gui():
+    ventana = tk.Tk()
+    ventana.title("Registro de Ventas")
+    ventana.geometry("400x300")
 
-    
+    tk.Label(ventana, text="Productos:").pack()
+    entrada_producto = tk.Entry(ventana)
+    entrada_producto.pack()
+
+    tk.Label(ventana, text="Precio:").pack()
+    entrada_precio = tk.Entry(ventana)
+    entrada_precio.pack()
+
+    def registrar_venta():
+        producto = entrada_producto.get()
+        precio = entrada_precio.get()
+
+        if not producto or not precio:
+            messagebox.showerror("Error", "Por favor, vuelva a internar.")
+            return
+        
+        agregar_venta(producto)
+        guardar_venta_en_txt(producto, precio)
+        messagebox.showinfo("Venta registrada")
+
+    tk.Button(ventana, text="Registrar Venta", command=registrar_venta).pack(pady=10)
+    ventana.mainloop()
