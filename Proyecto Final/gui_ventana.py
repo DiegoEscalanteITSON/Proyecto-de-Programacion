cb07cbeb734c0dde0e4e4f1b87b013008cc46597
DiegoEscalanteITSON <diego.escalante272259@potros.itson.edu.mx>
2025-11-27 12:11:25 -0700

    un cambio

--- Proyecto Final/gui_ventana.py
+++ Proyecto Final/gui_ventana.py
@@ -2,7 +2,7 @@
 import tkinter as tk
 from tkinter import messagebox, ttk
 from archivo_ventas import guardar_venta_en_txt
-from def_ventas import agregar_venta
+from archivo_ventas import agregar_venta
 
 def adrir_gui():
     ventana = tk.Tk()
