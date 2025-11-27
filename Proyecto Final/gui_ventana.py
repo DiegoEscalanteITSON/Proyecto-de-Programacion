e9c6feb9368de3fd27b58f1f0ba1a4308a97c956
DiegoEscalanteITSON <diego.escalante272259@potros.itson.edu.mx>
2025-11-26 22:08:05 -0700

    primeros import

--- Proyecto Final/gui_ventana.py
+++ Proyecto Final/gui_ventana.py
@@ -1,1 +1,5 @@
 # Archivo que abrira la ventana GUI para registra ventas
+import tkinter as tk
+from tkinter import messagebox, ttk
+from archivo_ventas import guardar_venta_en_txt
+from def_ventas import agregar_venta
