72dc3a6c1a3b101399e69acb79cfa6b583b468b1
ernestitoc900-cyber <ernestitoc900@gmail.com>
2025-11-27 08:49:48 -0700

    1

--- Proyecto Final/catalogo.py
+++ Proyecto Final/catalogo.py
@@ -1,9 +1,10 @@
 # Archivo del catalogo que ser leido
 
-Catalogo = (
-    {1, "nombre": "DOOM Eternal", "precio": 200},
-    {2, "nombre": "Hollow Knight", "precio": 150},
-    {3, "nombre": "Fallout New Vegas", "precio": 300},
-    {4, "nombre": "Civilization VI", "precio": 250},
-    {5, "nombre": "Super Smash Bros Ultimate", "precio": 400},
-)
+Catalogo = [
+    {"id": 1, "nombre": "DOOM Eternal", "precio": 200 },
+    {"id": 2, "nombre": "Hollow Knight", "precio": 150 },
+    {"id": 3, "nombre": "Fallout New Vegas", "precio": 300 },
+    {"id": 4, "nombre": "Civilization VI", "precio": 250 },
+    {"id": 5, "nombre": "Super Smash Bros Ultimate", "precio": 400 },
+    {"id": 6, "nombre": "The Witcher 3", "precio": 350 },   
+]
