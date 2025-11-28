87717aa14ce8fed31cfee354f552c1a3293b8a1c
DiegoEscalanteITSON <diego.escalante272259@potros.itson.edu.mx>
2025-11-28 09:15:08 -0700

    parte 2 de Main

--- Proyecto Final/main.py
+++ Proyecto Final/main.py
@@ -12,10 +12,24 @@ def menu():
     ventana.withdraw()
 
     while True:
-        print(Fore.CYAN + "\n=== Menú Principal ===")
-        print("1. Abrir registro de ventas")
-        print("2. Generar reporte de ventas")
-        print("3. Salir")
+        print(Fore.CYAN + """
+              \n=== Menú Principal ===
+        "1. Abrir registro de ventas"
+        "2. Generar reporte de ventas"
+        "3. Salir"
+              """)
+
+        opcion = input(Fore.YELLOW + "Seleccione una opción (1-3): ")
+        if opcion == '1':
+            adrir_gui()
+        elif opcion == '2':
+            generar_reporte_ventas()
+        elif opcion == '3':
+            print(Fore.GREEN + "Saliendo del programa. ¡Hasta luego!")
+            break
+        else:
+            print(Fore.RED + "Opción inválida. Por favor, intente de nuevo.")
+
 
 if __name__ == "__main__":
     adrir_gui()
