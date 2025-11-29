b1fa88b6455021557b5ba846d0599064cc3c6b08
DiegoEscalanteITSON <diego.escalante272259@potros.itson.edu.mx>
2025-11-28 19:04:44 -0700

    Ventas cambio

--- Proyecto Final/archivo_ventas.py
+++ Proyecto Final/archivo_ventas.py
@@ -1,21 +1,24 @@
 # Archivo que guarda la ventas en txt
-
-def guardar_venta_en_txt(ventas):
-    with open("ventas.txt", "w", encoding="utf-8") as archivo:
-        for venta in ventas:
-            archivo.write(f"{venta['nombre']} - ${venta['precio']}\n")
-            print(f"Venta guardada: {venta['nombre']} - ${venta['precio']}")
-            print("✔ Archivo ventas de registradas actualizado correctamente.")
-
-# Def que llamara a las funciones de ventas
-# ventas.py
+# Modulo que maneja la logica de las ventas
 
 ventas = []   # lista global
 
 def agregar_venta(juego, cantidad):
-    ventas.append((juego, cantidad))
-    print(f"Venta agregada: {juego} - Cantidad: {cantidad}")
+    """Agrega una venta a la lista de ventas.  """
+    ventas.append({"nombre": juego, "cantidad" cantidad})
+    print(f"✔ Venta agregada: {juego} - Cantidad: {cantidad}")
 
 def obtener_ventas():
     return ventas
 
+def guardar_venta_en_txt(ventas):
+    """Guarda las ventas en un archivo de texto."""
+    if not ventas:
+        print("No hay ventas para guardar.")
+        return
+    with open("ventas.txt", "w", encoding="utf-8") as archivo:
+        for venta in ventas:
+            archivo.write(f"{venta['nombre']} - ${venta['precio']}\n")
+            print(f"Venta guardada: {venta['nombre']} - ${venta['precio']}")
+
+print("✔ Archivo ventas de registradas actualizado correctamente.")
