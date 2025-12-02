dfb362d1b4f19c0475bfa0eb4bb9cdb2797d65b1
ernestitoc900-cyber <ernestitoc900@gmail.com>
2025-12-01 17:45:27 -0700

    nada

--- Proyecto Final/reporte_de_ventas.py
+++ Proyecto Final/reporte_de_ventas.py
@@ -10,19 +10,15 @@ def generar_reporte_ventas():
         print("No hay ventas registradas.")
         return
 
-    # Crear DataFrame a partir de la lista de dicts
     df = pd.DataFrame(ventas)
-    # Asegurarnos que existen las columnas necesarias
     df["cantidad"] = pd.to_numeric(df["cantidad"], errors='coerce').fillna(0).astype(int)
     df["precio_unitario"] = pd.to_numeric(df["precio_unitario"], errors='coerce').fillna(0.0).astype(float)
-    # Agrupar por juego y sumar las cantidades
     df["total"] = df["cantidad"] * df["precio_unitario"]
     reporte = df.groupby("juego").agg({"cantidad": "sum", "total": "sum"}).reset_index()
 
     print("\n==== Resumen de Ventas: ====")
     print(reporte)
 
-    # Graficar las ventas
     plt.figure(figsize=(10,6))
     plt.bar(reporte['juego'], reporte['cantidad'])
     plt.xlabel('Juegos')
@@ -40,3 +36,6 @@ def generar_reporte_ventas():
     plt.xticks(rotation=45, ha='right')
     plt.tight_layout()
     plt.show()
+
+
+
